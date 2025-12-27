481bc03c02ab436def817b87f8fcae27c9c2dd58
Umamaheswary Milleny <umamaheswarymillini@gmail.com>
2025-12-27 11:13:31 +0530

    implement 3D Prodcut Viewer Section

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import Navbar from "./components/Navbar.jsx";
 import Hero from "./components/Hero.jsx";
+import ProductViewer from "./components/ProductViewer.jsx";
+import gsap from "gsap";
+import {ScrollTrigger} from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
 const App = () => {
     return (
       <main>
           <Navbar/>
           <Hero/>
+          <ProductViewer/>
       </main>
     )
 }
