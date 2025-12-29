14e575f1f0fa245a724fe7c89388ca35fc1eaeca
Umamaheswary Milleny <umamaheswarymillini@gmail.com>
2025-12-29 17:15:37 +0530

    Implement Highlights Section

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,8 @@ import  Features from "./components/Features.jsx";
 // here gsap is imported even though not used here because this file settingup gsap globally it is like prepare gsap tools so child components can use them
 import gsap from "gsap";
 import {ScrollTrigger} from "gsap/ScrollTrigger";
+import Highlights from "./components/Highlights.jsx";
+
 
 gsap.registerPlugin(ScrollTrigger);
 // registerplugin is done in app becasue it only run once becasue it is the root of our app and registering plugin once is enough for entire app
@@ -21,6 +23,7 @@ const App = () => {
           <Showcase/>
           <Performance/>
           <Features/>
+          <Highlights/>
       </main>
     )
 }
